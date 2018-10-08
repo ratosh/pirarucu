5733e18c8f65d40a14d6cbc0b19c67e525a5c81f
Ratosh <ratosh@gmail.com>
2018-10-08 19:05:21 -0300

    Benchmark

--- pirarucu-util/src/main/kotlin/pirarucu/benchmark/BenchmarkApplication.kt
+++ pirarucu-util/src/main/kotlin/pirarucu/benchmark/BenchmarkApplication.kt
@@ -1,18 +1,9 @@
 package pirarucu.benchmark
 
-import pirarucu.search.SearchConstants
-
 fun main(args: Array<String>) {
     var speed = LongArray(1)
     for (tries in 0 until speed.size) {
         speed[tries] = Benchmark.runBenchmark(Benchmark.DEFAULT_BENCHMARK_DEPTH)
-    }
-    for (index in SearchConstants.hits.indices) {
-        if (SearchConstants.tries[index] > 0) {
-            println("$index " + SearchConstants.hits[index] * 1_000 / SearchConstants.tries[index])
-        } else {
-            println("$index unknown")
-        }
     }
     println("Time taken " + speed.sum())
     println("Max " + speed.max()!!)
